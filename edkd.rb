c9d2b5539e20b5247a6cf4aad747324253f244d7
YKatsuy <dogoru@yamauchi.local>
2021-07-13 10:57:17 +0900

    update homebrew

--- edkd.rb
+++ edkd.rb
@@ -3,7 +3,7 @@ VERSION="1.0.0"
 class Edkd < Formula
   desc "Calculate the distance from the latitude(ed) and longitude(kd) of two points"
   homepage "https://github.com/Ykatsuy/edkd"
-  url "https://github.com/tamada/edkd/releases/download/v#{VERSION}/edkd-#{VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/tamada/edkd/releases/download/#{VERSION}/edkd-#{VERSION}_darwin_amd64.tar.gz"
   version VERSION
   license "MIT License"
 
