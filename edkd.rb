3ea4999391b8150f8eb51ace19d5ae0c209d5d18
YKatsuy <dogoru@yamauchi.local>
2021-07-26 17:23:51 +0900

    update brew

--- edkd.rb
+++ edkd.rb
@@ -6,6 +6,7 @@ class Edkd < Formula
   #url "https://github.com/YKatsuy/edkd/releases/download/#{VERSION}/edkd-#{VERSION}_darwin_amd64.tar.gz"
   url "https://github.com/YKatsuy/edkd/releases/download/#{VERSION}/edkd-#{VERSION}_darwin_amd64.tar.gz"
   version VERSION
+  sha256 "71c7cb8fe526da0332ec8f82507cfd88108f62e7a05feb88c759714ab07c7884"
   license "MIT License"
 
   option "without-completions", "Disable bash completions"
